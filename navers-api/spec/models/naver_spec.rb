f49ebde95cf41dbf65658c377e834678c3ec4fbd
George Borsato <georginhoborsato@gmail.com>
2021-03-19 10:40:30 -0300

    teste unit model naver

--- navers-api/spec/models/naver_spec.rb
+++ navers-api/spec/models/naver_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe Naver, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'Should fail if name is empty' do
+    naver = build(:naver, name: nil)
+    expect(naver).to_not be_valid 
+  end
+
+  it 'Should fail if birthdate is empty ' do
+    naver = build(:naver, birthdate: nil)
+    expect(naver).to_not be_valid 
+  end
+
+  it 'Should fail is admission_date is empty ' do
+    naver = build(:naver, adminssion_date: nil)
+    expect(naver).to_not be_valid 
+  end
+
+  it 'Should fails is job_role is empty' do
+    naver = build(:naver, job_role: nil)
+    expect(naver).to_not be_valid 
+  end
+
+  it 'Should fails is user_id is empty' do
+    naver = build(:naver, user_id: nil)
+    expect(naver).to_not be_valid
+  end
+
 end
