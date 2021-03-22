d5fc7eae6856d07e00fe37890634b42164f6e786
George Borsato <georginhoborsato@gmail.com>
2021-03-22 15:25:53 -0300

    update the file

--- navers-api/app/controllers/sessions_controller.rb
+++ navers-api/app/controllers/sessions_controller.rb
@@ -1,19 +1,8 @@
 class SessionsController < Devise::SessionsController
-  clear_respond_to
+  
   respond_to :json
 
-  
-
-
   private
-
-  #def respond_with(resource, _opts = {})
-    #super do |resource|
-      #if user_signed_in?
-        #render json: resource.token = encode(user_id: current_user.id)
-      #end
-    #end
-  #end
 
   def respond_with(resource, _opts = {})
 
@@ -26,11 +15,4 @@ class SessionsController < Devise::SessionsController
     head :no_content
   end
 
-  def encode(payload, expiration = nil)
-    expiration ||= Rails.application.secrets.jwt_expiration_hours
-    payload = payload.dup
-    payload['exp'] = expiration.to_i.hours.from_now.to_i
-
-    JWT.encode payload, Rails.application.secrets.jwt_secret
-  end
 end
